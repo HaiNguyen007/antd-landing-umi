b699ee9061645f5af5ccf946defea2a638333faf
Hai Nguyen <hai@ahijsc.com>
2021-08-09 23:00:07 +0700

    fix /src/pages/index.js

--- js/src/pages/index.js
+++ js/src/pages/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import styles from './index.css';
+import Home from '../Home';
 
 export default function Page() {
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
+	  <Home />
   );
 }
